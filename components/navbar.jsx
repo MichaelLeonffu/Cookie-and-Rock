3703d55c49c7bb725ef0e8a39b77546b2562a621
Michael Leonffu <leonffu.michael@gmail.com>
2021-04-12 00:04:14 -0700

    Fixed state for menu button dropdown

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -7,16 +7,17 @@ import React, { useState } from 'react';
 export default function Navbar({ children }) {
 
   const [ddmenu, setddmenu] = useState(false);
+  const [navmenu, setnavmenu] = useState(false);
 
   return (
     <>
-    {/* // << !--This example requires Tailwind CSS v2.0 + --> */}
+      {/* // << !--This example requires Tailwind CSS v2.0 + --> */}
       <nav className="bg-gray-800">
         <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
           <div className="relative flex items-center justify-between h-16">
             <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
               {/* <!-- Mobile menu button--> */}
-              <button type="button" className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
+              <button type="button" className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false" onClick={() => setnavmenu(!navmenu)}>
                 <span className="sr-only">Open main menu</span>
                 {/* <!--
               Icon when menu is closed.
@@ -25,7 +26,7 @@ export default function Navbar({ children }) {
 
               Menu open: "hidden", Menu closed: "block"
             --> */}
-                <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                <svg className={`h-6 w-6 ${navmenu ? ' hidden' : 'block'}`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
                 </svg>
                 {/* <!--
@@ -35,7 +36,7 @@ export default function Navbar({ children }) {
 
               Menu open: "block", Menu closed: "hidden"
             --> */}
-                <svg className="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                <svg className={`h-6 w-6 ${!navmenu ? ' hidden' : 'block'}`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
                 </svg>
               </button>
@@ -63,8 +64,8 @@ export default function Navbar({ children }) {
             <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
               {/* <button className="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
                 <span className="sr-only">View notifications</span> */}
-                {/* <!-- Heroicon name: outline/bell --> */}
-                {/* <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+              {/* <!-- Heroicon name: outline/bell --> */}
+              {/* <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
                 </svg>
               </button> */}
@@ -89,7 +90,7 @@ export default function Navbar({ children }) {
                 From: "transform opacity-100 scale-100"
                 To: "transform opacity-0 scale-95"
             --> */}
-                <div className={`${ ddmenu ? ' ' : 'hidden' }`}>
+                <div className={`${ddmenu ? ' ' : 'hidden'}`}>
                   <div className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none transition ease-out duration-100 transform opacity-0 scale-95 transform opacity-100 scale-100" role="menu" aria-orientation="vertical" aria-labelledby="user-menu">
                     <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Your Profile</a>
                     <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Settings</a>
@@ -102,7 +103,7 @@ export default function Navbar({ children }) {
         </div>
 
         {/* <!-- Mobile menu, show/hide based on menu state. --> */}
-        <div className="sm:hidden" id="mobile-menu">
+        <div className={`sm:hidden ${navmenu ? ' block' : 'hidden'}`} id="mobile-menu">
           <div className="px-2 pt-2 pb-3 space-y-1">
             {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
             <a href="#" className="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium" aria-current="page">Home</a>
